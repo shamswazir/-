6687ac68b9972e5ab2885e742441b15c93a421c3
shamswazir <shamswazir780@gmail.com>
2020-12-29 23:09:31 +0530

    geting images

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
     <>     
     <Navbar/>
     <Switch>
-     <Route component={Home} />
+    
      <Route exact path='/rooms/' component={Rooms} />
      <Route exact path='/rooms/:slug' component={SingleRoom} />
-   
+     <Route  component={Home} />
     </Switch>
     </>
   );
