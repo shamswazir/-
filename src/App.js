d31302276fbe1b986567bb2087eae26793301c05
shamswazir <shamswazir780@gmail.com>
2020-12-29 22:44:45 +0530

    some change

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import React from 'react';
 import Home from './Pages/Home';
 import Rooms from './Pages/Room';
 import SingleRoom from './Pages/SingleRoom';
-import Error from './Pages/Error';
 import Navbar from './components/Navbar';
 
 import {Route , Switch} from 'react-router-dom';
